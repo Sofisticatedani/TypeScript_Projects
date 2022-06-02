1b00b98dcb5768cea6b71b4c745ff475aece9ab1
Sofisticatedani <42324586+Sofisticatedani@users.noreply.github.com>
2022-06-02 14:36:40 +0100

    Add files via upload

--- Todo app list/todo.js
+++ Todo app list/todo.js
@@ -1,13 +1,19 @@
 // add button
 var addtask = function () {
+    //create list  
     var list = document.createElement("li");
     var inputValue = document.querySelector("#newtask");
     var task = document.createTextNode(inputValue.value);
-    list.appendChild(task);
     if (inputValue.value === "") {
         alert("Your task can not be empty");
     }
     else {
+        // create checkbox
+        var checkbox = document.createElement("input");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.className = "check";
+        list.appendChild(checkbox);
+        list.appendChild(task);
         document.querySelector("#item-list").appendChild(list);
         inputValue.value = "";
     }
@@ -27,6 +33,36 @@ var addtask = function () {
     };
     for (var i = 0; i < close.length; i++) {
         _loop_1(i);
+    }
+    // edit list
+    var editbtn = document.createElement("img");
+    editbtn.setAttribute("src", "download.png");
+    editbtn.setAttribute("width", "10px");
+    editbtn.setAttribute("height", "10px");
+    editbtn.className = "edit";
+    list.appendChild(editbtn);
+    document.querySelector("#item-list").appendChild(list);
+    var edits = document.getElementsByClassName("edit");
+    var _loop_2 = function (i) {
+        edits[i].onclick = function () {
+            var div = edits[i].parentElement;
+            inputValue.value = div.innerText;
+            div.style.display = "none";
+        };
+    };
+    for (var i = 0; i < edits.length; i++) {
+        _loop_2(i);
+    }
+    //check list
+    var checkie = document.getElementsByClassName("check");
+    var _loop_3 = function (i) {
+        checkie[i].onclick = function () {
+            var div = checkie[i].parentElement;
+            div.style.textDecoration = "line-through";
+        };
+    };
+    for (var i = 0; i < checkie.length; i++) {
+        _loop_3(i);
     }
 };
 //Eventlistener for enter button
--- Todo app list/todo.ts
+++ Todo app list/todo.ts
@@ -1,15 +1,24 @@
 // add button
 const addtask =() => {
+    //create list  
     let list = document.createElement("li");
     let inputValue = document.querySelector("#newtask") as HTMLInputElement;
-    var task = document.createTextNode(inputValue.value);
-    list.appendChild(task);
+    var task= document.createTextNode(inputValue.value);
+    
+    
 
     if(inputValue.value === ""){
         alert("Your task can not be empty");
     }
 
     else{
+        // create checkbox
+        let checkbox = document.createElement("input") as HTMLInputElement;
+        checkbox.setAttribute ("type","checkbox");
+        checkbox.className = "check";
+        list.appendChild(checkbox);
+        list.appendChild(task);
+       
         document.querySelector("#item-list").appendChild(list);
         inputValue.value = "";
     }
@@ -34,7 +43,39 @@ const addtask =() => {
         }
         
     }
+// edit list
+    let editbtn = document.createElement("img");
+    editbtn.setAttribute("src","download.png");
+    editbtn.setAttribute("width","10px");
+    editbtn.setAttribute("height","10px");
+
+    editbtn.className = "edit";
+    list.appendChild(editbtn);
+    document.querySelector("#item-list").appendChild(list);
+
+    let edits = document.getElementsByClassName("edit") as HTMLCollectionOf<HTMLElement>;
+        
+
+    for(let i=0;i<edits.length;i++){
+
+        edits[i].onclick = function(){
+            let div = edits[i].parentElement;
+            inputValue.value = div.innerText;
+            div.style.display="none";
+        }
+    }
+
+    //check list
+    let checkie = document.getElementsByClassName("check") as HTMLCollectionOf<HTMLElement>;
+    for(let i=0;i<checkie.length;i++){
+
+        checkie[i].onclick = function(){
+            let div = checkie[i].parentElement;
+            div.style.textDecoration="line-through";
+        }
+    }
 }
+
 
 //Eventlistener for enter button
 addEventListener('keypress',(event)=> {
